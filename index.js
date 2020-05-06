50991e7f6f203f6b74725ab08e508a22511b3a17
Choi Jiwon <winning1993@naver.com>
2020-05-07 08:53:05 +0900

    기능구현(javascript) 완성

--- index.js
+++ index.js
@@ -3,25 +3,30 @@ const countrySelect2 = document.getElementById("countrySelect2");
 const swapBtn = document.getElementById("swapBtn");
 const amount = document.getElementById("amount");
 const amount2 = document.getElementById("amount2");
+const example = document.getElementById("example");
 
 let baseCountry = "KRW";
 let objectCountry = countrySelect2.value;
-
-console.log(objectCountry)
+let rate = 0;
 
 function getData(){
     fetch(`https://prime.exchangerate-api.com/v5/904982415b4b0ed8d1ae2bc5/latest/${baseCountry}`).then(function(res, req){
         res.json().then(function(data){
-            console.log(data.conversion_rates[objectCountry])
+            rate = data.conversion_rates[objectCountry];
+            amount2.value = amount.value * rate;
+            example.innerHTML = `1 ${baseCountry} = ${rate} ${objectCountry}`;
         })
         
     })
 }
 
 function handleConvert(){
+    baseCountry = countrySelect2.value;
+    objectCountry = countrySelect.value;
     let saved = countrySelect.value;
     countrySelect.value = countrySelect2.value;
     countrySelect2.value = saved;
+    getData();
 }
 
 function handleSelect(){
@@ -35,7 +40,7 @@ function handleSelect2(){
 }
 
 function handleTyping(){
-    amount2.value = amount.value
+    getData();
 }
 
 function init(){
@@ -43,7 +48,9 @@ function init(){
     countrySelect2.addEventListener("change", handleSelect2);
     swapBtn.addEventListener("click", handleConvert);
     amount.addEventListener("keydown", handleTyping);
+    amount.addEventListener("change", handleTyping);
     getData();
+    
 }
 
 init();
